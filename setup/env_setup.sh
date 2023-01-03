ab7652be4f786395125c7ecc01cfea6d5f2ba303
Josh <jolalemi@gmail.com>
2023-01-03 04:28:15 +0100

    Added bash script

---
+++ setup/env_setup.sh
@@ -0,0 +1,22 @@
+# Setup the work environment
+
+## Clone the repository in your present working directory
+git clone https://github.com/JoshOlam/MAONTECH_ML_Assessment.git
+
+## Move the present working directory into the git repository
+cd MAONTECH_ML_Assessment
+
+## Create a virtual environment in the current directory
+python3 -m venv maon_venv
+
+## Activate the environment (windows)
+#maon_venv/Scripts/activate.bat
+
+## Activate the environment (Linux or MacOS)
+source maon_venv/bin/activate
+
+## Install the requirement packages in the working environment
+pip install requirements.txt
+
+## Create a jupyter kernel
+python -m ipykernel install --user --name=maonKernel
