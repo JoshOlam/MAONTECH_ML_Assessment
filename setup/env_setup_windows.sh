23af8e46efeaca0a98647fcc5f9bf284032ff947
Joshua Olalemi <jolalemi@gmail.com>
2023-01-03 13:51:23 +0100

    Update env_setup_windows.sh

--- setup/env_setup_windows.sh
+++ setup/env_setup_windows.sh
@@ -1,10 +1,10 @@
 # Setup the work environment
 
 ## Clone the repository in your present working directory
-git clone https://github.com/JoshOlam/MAONTECH_ML_Assessment.git
+#git clone https://github.com/JoshOlam/MAONTECH_ML_Assessment.git
 
 ## Move the present working directory into the git repository
-cd MAONTECH_ML_Assessment
+#cd MAONTECH_ML_Assessment
 
 ## Create a virtual environment in the current directory
 python3 -m venv maon_venv
